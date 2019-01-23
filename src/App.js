f9018a3d822f7f585b302ad0986bd1db8b10098b
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2019-01-23 14:59:50 -0200

    first commit

--- src/App.js
+++ src/App.js
@@ -13,42 +13,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <form onSubmit={e => e.preventDefault()}>
-        <Form horizontal>
-          <FormGroup >
-            <Col componentClass={ControlLabel} sm={2}>
-              Nome
-            </Col>
-            <Col sm={10}>
-              <FormControl type="text" placeholder="Nome" />
-            </Col>
-          </FormGroup>
-
-          <FormGroup>
-            <Col componentClass={ControlLabel} sm={2}>
-              Telefone
-            </Col>
-            <Col sm={10}>
-              <FormControl type="text" placeholder="Telefone" />
-            </Col>
-          </FormGroup>
-
-          <FormGroup>
-            <Radio name="radioGroup" inline>
-              M
-            </Radio>{' '}
-            <Radio name="radioGroup" inline>
-              F
-            </Radio>{' '}
-          </FormGroup>
-
-          <FormGroup>
-            <Col smOffset={2} sm={10}>
-              <Button bsStyle="primary" type="submit">Sign in</Button>
-            </Col>
-          </FormGroup>
-        </Form>
-      </form>
+      <h1>Hello World</h1>
     );
   }
 }
